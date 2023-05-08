df895d4c2ea0618dc2248d0c8e1592913bc9e719
Roman_B <batrakov.r.v@yandex.ru>
2023-05-08 22:24:28 +0300

    Refactor: add doc

--- algorithms/q14_longest_common_prefix/LongestCommonPrefix.java
+++ algorithms/q14_longest_common_prefix/LongestCommonPrefix.java
@@ -1,6 +1,18 @@
 package q14_longest_common_prefix;
 
-import java.util.Objects;
+/**
+ *  Write a function to find the longest common prefix string amongst an array of strings.
+ *  If there is no common prefix, return an empty string "".
+ *
+ *  Example 1:
+ *  Input: strs = ["flower","flow","flight"]
+ *  Output: "fl"
+ *
+ *  Example 2:
+ *  Input: strs = ["dog","racecar","car"]
+ *  Output: ""
+ *  Explanation: There is no common prefix among the input strings.
+ */
 
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
