5521a6bfa400264c1c26686e901ff305ba628b1a
Roman_B <batrakov.r.v@yandex.ru>
2023-12-31 12:28:50 +0400

    26

--- algorithms/q26_remove_duplicates_from_sorted_array/RemoveDuplicatesFromSortedArray.java
+++ algorithms/q26_remove_duplicates_from_sorted_array/RemoveDuplicatesFromSortedArray.java
@@ -1,9 +1,5 @@
 package q26_remove_duplicates_from_sorted_array;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
  *
